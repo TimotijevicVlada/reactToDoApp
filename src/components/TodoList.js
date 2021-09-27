a5a2da8d632915d606c96b5e2bb3e216a3962f44
Vladimir <timotijevicvlada@gmail.com>
2021-09-27 20:00:16 +0200

    Rewritting map function in TodosList to solve bug

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,7 +5,7 @@ function TodoList({ todos, setTodos, filteredTodos }) {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        {filteredTodos.map((todo) => (
+        {filteredTodos.map(todo => (
           <Todo
             setTodos={setTodos}
             todos={todos}
